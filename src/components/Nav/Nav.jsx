f14c8652422d1ee51be9127503f401c2bf94d556
Maya Peters <mayampeters@gmail.com>
2023-08-03 12:54:35 -0500

    removes old nav bar

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -11,7 +11,6 @@ function Nav() {
   const user = useSelector((store) => store.user);
 
   return (
-    <>
     <AppBar position='static'>
       <Toolbar>
         <Link to='/home'>
@@ -30,41 +29,6 @@ function Nav() {
         )}
       </Toolbar>
     </AppBar>
-   
-    <div className="nav">
-      <Link to="/home">
-        <h2 className="nav-title">Prime Solo Project</h2>
-      </Link>
-      <div>
-        {/* If no user is logged in, show these links */}
-        {!user.id && (
-          // If there's no user, show login/registration links
-          <Link className="navLink" to="/login">
-            Login / Register
-          </Link>
-        )}
-
-        {/* If a user is logged in, show these links */}
-        {user.id && (
-          <>
-            <Link className="navLink" to="/user">
-              Home
-            </Link>
-
-            <Link className="navLink" to="/info">
-              Info Page
-            </Link>
-
-            <LogOutButton className="navLink" />
-          </>
-        )}
-
-        <Link className="navLink" to="/about">
-          About
-        </Link>
-      </div>
-    </div>
-    </>
   );
 }
 
