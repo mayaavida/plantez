4a7c66fad0d4c176bb149bf3ea7269f6c5964f34
Maya Peters <mayampeters@gmail.com>
2023-08-11 19:14:09 -0500

    adds styling to userpage

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -1,14 +1,19 @@
 import React from "react";
 import LogOutButton from "../LogOutButton/LogOutButton";
 import { useSelector } from "react-redux";
-import { useState, useEffect } from 'react';
-import { Box, Typography } from "@mui/material";
+import {
+  Box,
+  Typography,
+  Card,
+  CardContent,
+  CardMedia,
+  CardActions,
+} from "@mui/material";
 
 function UserPage() {
   const user = useSelector((store) => store.user);
   const userPlants = useSelector((store) => store.userPlants);
   console.log(userPlants);
-
 
   return (
     <Box>
@@ -18,9 +23,33 @@ function UserPage() {
         </Typography>
       </Box>
       <Box display="flex">
-        <Box>
+        <Box display="flex" flexWrap='wrap' gap={3} margin={3} padding={1} sx={{border: 1, borderRadius:1, borderColor:'primary'}}>
+          {userPlants.map((plant) => (
+            <Card key={plant.nickname}>
+              <CardContent>
+                <Typography component="h5" variant="h5">
+                  {plant.nickname}
+                </Typography>
+              </CardContent>
+              {plant.image_url ? (
+                <CardMedia
+                  component="img"
+                  height="150"
+                  src={plant.image_url}
+                  alt={"Image of a household plant"}
+                ></CardMedia>
+              ) : (
+                <Typography variant="span" component="div">
+                  No image available
+                </Typography>
+              )}
+            </Card>
+          ))}
         </Box>
-        <Box>
+        <Box display='flex' flexWrap='wrap' flexDirection='column' gap={3} margin={3} padding={1} sx={{border: 1, borderRadius:1, borderColor:'primary'}}>
+          <Typography component='div' variant="h3">
+            Total Plants: {userPlants.length}
+          </Typography>
           <LogOutButton />
         </Box>
       </Box>
