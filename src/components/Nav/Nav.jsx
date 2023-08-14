1d89285103571eaaa59c5a032e1f0d856f96bb4c
Maya Peters <mayampeters@gmail.com>
2023-08-14 11:47:27 -0500

    adds styling to nav bar

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -1,31 +1,50 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
-import LogOutButton from '../LogOutButton/LogOutButton';
-import './Nav.css';
-import { useSelector } from 'react-redux';
-import { Box, AppBar, Typography, Button, IconButton, Toolbar, Input } from '@mui/material';
-import planteez from '../../images/planteez.png';
-import InputIcon from '@mui/icons-material/Input';
+import React from "react";
+import { Link, useHistory } from "react-router-dom";
+import LogOutButton from "../LogOutButton/LogOutButton";
+import { useSelector } from "react-redux";
+import {
+  Box,
+  AppBar,
+  Typography,
+  Button,
+  IconButton,
+  Toolbar,
+  Input,
+} from "@mui/material";
+import planteez from "../../images/planteez.png";
+import InputIcon from "@mui/icons-material/Input";
 
 function Nav() {
   const user = useSelector((store) => store.user);
+  const history = useHistory();
 
   return (
-    <AppBar position='static' >
-      <Toolbar sx={{justifyContent: 'space-between'}}>
-        <Link to='/home'>
-          <img src={planteez} alt='PlantEZ logo' height={25} />
+    <AppBar position="static">
+      <Toolbar sx={{ justifyContent: "space-between" }}>
+        <Link to="/home">
+          <img src={planteez} alt="PlantEZ logo" height={25} />
         </Link>
         {!user.id && (
           // If there's no user, show login/registration links
-          <Button variant='contained' bgcolor='secondary' startIcon={<InputIcon />}>
-            <Link to="/login">
-              Login / Register
-            </Link>
+          <Button
+            variant="contained"
+            bgcolor="secondary"
+            onClick={()=>history.push('/login')}
+          >
+            Login or Register
           </Button>
         )}
-           {user.id && (
-            <LogOutButton/>
+        {user.id && (
+          <>
+            <Button
+              variant="contained"
+              bgcolor="secondary"
+              onClick={()=>history.push('/user')}
+            >
+              My Plants
+            </Button>
+            <LogOutButton />
+          </>
         )}
       </Toolbar>
     </AppBar>
